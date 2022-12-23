147bb318e4a31415691322632765fe1c95dabcfa
pyz <2608779179@qq.com>
2022-12-23 20:31:17 +0800

    main commit1

--- src/main/java/com/puge/git/GitTest.java
+++ src/main/java/com/puge/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("Git Test!");
         System.out.println("Git Test2!");
         System.out.println("Git Test3!");
+        System.out.println("main Git Test3!");
     }
 }
