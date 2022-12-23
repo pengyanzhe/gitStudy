d4888e29d6b0021f254bdf392fa454a86140e1f6
pyz <2608779179@qq.com>
2022-12-23 19:47:05 +0800

    third commit

--- src/main/java/com/puge/git/GitTest.java
+++ src/main/java/com/puge/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Test!");
         System.out.println("Git Test2!");
+        System.out.println("Git Test3!");
     }
 }
