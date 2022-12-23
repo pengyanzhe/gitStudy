e30c456aadf8f2d9c4e37436223428a79b23d6b1
pyz <2608779179@qq.com>
2022-12-23 19:46:31 +0800

    second commit

--- src/main/java/com/puge/git/GitTest.java
+++ src/main/java/com/puge/git/GitTest.java
@@ -6,5 +6,6 @@ package com.puge.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Test!");
+        System.out.println("Git Test2!");
     }
 }
